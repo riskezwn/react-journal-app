b7fd73b82848e5c6612cbac13123758498a78bdf
Riskezwn <enriquegarciagasc@gmail.com>
2022-02-26 13:12:34 +0100

    note screen style

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,25 @@ export const NoteScreen = () => {
     return (
         <div className="notes__main-content">
             <NotesAppBar />
+            <div className="notes__content">
+                <input
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+                <textarea
+                    placeholder="What happened today?"
+                    className="notes__textarea"
+                    autoComplete="off"
+                ></textarea>
+                <div className="notes__image">
+                    <img
+                        src="https://i.blogs.es/0ca5da/ambulo_polar_wide/840_560.jpg"
+                        alt="imagen"
+                    />
+                </div>
+            </div>
         </div>
     );
 };
