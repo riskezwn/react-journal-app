528d0dd5fa9fd5a0764ecc04a45bd9d18cdbbb19
Riskezwn <enriquegarciagasc@gmail.com>
2022-02-26 17:10:20 +0100

    login dispatch

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,17 +1,36 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { login } from "../../actions/auth";
+import { useForm } from "../../hooks/useForm";
 
 export const LoginScreen = () => {
+
+    const dispatch = useDispatch();
+
+    const [formValues, handleInputChange] = useForm({
+        email: "riskezwn@gmail.com",
+        password: "123456",
+    });
+    const { email, password } = formValues;
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        dispatch(login(12345, 'riskezwn'));
+    }
+
     return (
         <div>
             <h2 className="auth__title">Login</h2>
-            <form>
+            <form onSubmit={handleLogin}>
                 <input
                     type="email"
                     placeholder="john@doe.com"
                     name="email"
                     className="auth__input"
                     autoComplete="off"
+                    value={email}
+                    onChange={handleInputChange}
                 />
                 <input
                     type="password"
@@ -19,11 +38,10 @@ export const LoginScreen = () => {
                     name="password"
                     className="auth__input"
                     autoComplete="off"
+                    value={password}
+                    onChange={handleInputChange}
                 />
-                <button
-                    type="submit"
-                    className="btn btn-primary btn-block"
-                >
+                <button type="submit" className="btn btn-primary btn-block">
                     Login
                 </button>
                 <hr />
@@ -42,7 +60,9 @@ export const LoginScreen = () => {
                         </p>
                     </div>
                 </div>
-                <Link to="/auth/register" className="link">Create new account</Link>
+                <Link to="/auth/register" className="link">
+                    Create new account
+                </Link>
             </form>
         </div>
     );
