66f0153778095852867e553aba2a52193ee2226b
Riskezwn <enriquegarciagasc@gmail.com>
2022-03-02 13:45:50 +0100

    show name in sidebar

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { startLogout } from "../../actions/auth";
 import { JournalEntries } from "./JournalEntries";
 
 export const Sidebar = () => {
     const dispatch = useDispatch();
 
+    const { name } = useSelector((state) => state.auth);
+
     const handleLogout = () => {
         dispatch(startLogout());
-        console.log("click");
     };
 
     return (
@@ -16,7 +17,7 @@ export const Sidebar = () => {
             <div className="journal__sidebar-navbar">
                 <h3 className="mt-5">
                     <i className="far fa-moon"></i>
-                    <span> riskezwn</span>
+                    <span> {name}</span>
                 </h3>
                 <button className="btn" onClick={handleLogout}>
                     Logout
