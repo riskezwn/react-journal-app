8e4531a779e628344b3693d57e0fa569c8449c75
Riskezwn <enriquegarciagasc@gmail.com>
2022-03-05 18:47:06 +0100

    add new note to state

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -22,11 +22,20 @@ export const startNewNote = () => {
         const collRef = collection(db, `${uid}/journal/notes`);
         const doc = await addDoc(collRef, newNote);
         dispatch(activeNote(doc.id, newNote));
+        dispatch(addNewNote(doc.id, newNote));
     };
 };
 
 export const activeNote = (id, note) => ({
     type: types.notesActive,
+    payload: {
+        id,
+        ...note,
+    },
+});
+
+export const addNewNote = (id, note) => ({
+    type: types.notesAddNew,
     payload: {
         id,
         ...note,
