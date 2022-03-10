93c265678070560b0c875b51f2a690ab085135d4
Riskezwn <enriquegarciagasc@gmail.com>
2022-03-10 12:52:17 +0100

    fileUpload test

---
+++ src/tests/helpers/fileUpload.test.js
@@ -0,0 +1,22 @@
+import { fileUpload } from "../../helpers/fileUpload";
+
+describe("test in fileUpload", () => {
+    test("should upload an image and return URL", async () => {
+        const resp = await fetch(
+            "https://www.collinsdictionary.com/images/full/mountain_221506423.jpg"
+        );
+        const blob = await resp.blob();
+
+        const file = new File([blob], "photo.png");
+        const url = await fileUpload(file);
+
+        expect(typeof url).toBe("string");
+    });
+
+    test("should return an error", async () => {
+        const file = new File([], "photo.png");
+        const url = await fileUpload(file);
+
+        expect(url).toBe(null);
+    });
+});
