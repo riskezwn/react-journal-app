850cc16e986f5830302b658d07c33048ad67d6fa
riskezwn <enriquegarciagasc@gmail.com>
2022-03-18 13:03:21 +0100

    delete image in tests

--- src/tests/helpers/fileUpload.test.js
+++ src/tests/helpers/fileUpload.test.js
@@ -1,22 +1,26 @@
 import { fileUpload } from "../../helpers/fileUpload";
 
 describe("test in fileUpload", () => {
-    test("should upload an image and return URL", async () => {
-        const resp = await fetch(
-            "https://www.collinsdictionary.com/images/full/mountain_221506423.jpg"
-        );
-        const blob = await resp.blob();
+  test("should upload an image and return URL", async () => {
+    const resp = await fetch(
+      "https://www.collinsdictionary.com/images/full/mountain_221506423.jpg"
+    );
+    const blob = await resp.blob();
 
-        const file = new File([blob], "photo.png");
-        const url = await fileUpload(file);
+    const file = new File([blob], "photo.png");
+    const url = await fileUpload(file);
 
-        expect(typeof url).toBe("string");
-    });
+    expect(typeof url).toBe("string");
 
-    test("should return an error", async () => {
-        const file = new File([], "photo.png");
-        const url = await fileUpload(file);
+    //  Borrar imagen
+    // const segments = url.split("/");
+    // const imgId = segments[segments.length - 1].replace(".png", "");
+  });
 
-        expect(url).toBe(null);
-    });
+  test("should return an error", async () => {
+    const file = new File([], "photo.png");
+    const url = await fileUpload(file);
+
+    expect(url).toBe(null);
+  });
 });
